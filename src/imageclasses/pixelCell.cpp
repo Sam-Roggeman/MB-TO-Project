20181f6b89069292ea9c1d6760bd66935c2ac0eb
Sam Roggeman <Sam.Roggeman@gmail.com>
2022-01-27 15:42:37 +0100

    added functionality for directioncell (orange), startcell (Red), finish (green), checkpoint (yellow) to pixelCell.h

--- src/imageclasses/pixelCell.cpp
+++ src/imageclasses/pixelCell.cpp
@@ -3,7 +3,35 @@
 //
 
 #include "pixelCell.h"
+
 pixelCell::pixelCell(celltype type, const Color& c) : type(type), c(c) {}
-pixelCell::pixelCell(celltype type, Color&& c) : type(type), c(std::move(c)) {}
+pixelCell::pixelCell(celltype type, Color&& c) : type(type), c(c) {}
+celltype pixelCell::getType() const { return type; }
 Wall::Wall() : pixelCell(WallCell, Color(255, 248, 220)) {}
 Road::Road() : pixelCell(RoadCell, Color(220, 220, 220)) {}
+Direction::Direction() : pixelCell(DirectionCell, Color(255, 167, 73)) {}
+Finish::Finish() : pixelCell(FinishCell, Color(117, 255, 79)) {}
+Start::Start() : pixelCell(StartCell, Color(255, 28, 43)) {}
+CheckPoint::CheckPoint(unsigned int row, unsigned int col, int _id)
+    : pixelCell(CheckPointCell, Color(255, 233, 127)), id(_id), rightmost_point({col, row}), bottem_point({col, row}),
+      leftmost_point({col, row}), highest_point({col, row})
+{
+}
+void CheckPoint::updateExtrema(unsigned int row, unsigned int col)
+{
+        if (row < highest_point.second) {
+                highest_point = {col, row};
+        } else if (row > bottem_point.second) {
+                bottem_point = {col, row};
+        }
+        if (col < leftmost_point.first) {
+                leftmost_point = {col, row};
+        } else if (col > rightmost_point.first) {
+                rightmost_point = {col, row};
+        }
+}
+unsigned int CheckPoint::getId() const { return id; }
+const std::pair<unsigned int, unsigned int>& CheckPoint::getP1() const { return leftmost_point; }
+const std::pair<unsigned int, unsigned int>& CheckPoint::getP2() const { return bottem_point; }
+const std::pair<unsigned int, unsigned int>& CheckPoint::getP3() const { return rightmost_point; }
+const std::pair<unsigned int, unsigned int>& CheckPoint::getP4() const { return highest_point; }
--- src/imageclasses/pixelCell.h
+++ src/imageclasses/pixelCell.h
@@ -10,18 +10,23 @@ enum celltype
 {
         WallCell,
         RoadCell,
-        AirCell
+        AirCell,
+        DirectionCell,
+        StartCell,
+        FinishCell,
+        CheckPointCell
 };
 
 class pixelCell
 {
 protected:
-        celltype type = WallCell;
+        const celltype type;
         Color c{};
 
 public:
         virtual ~pixelCell() = default;
         pixelCell(celltype type, const Color& c);
+        celltype getType() const;
         pixelCell(celltype type, Color&& c);
         virtual std::string toString() const = 0;
         friend std::ostream& operator<<(std::ostream& out, const pixelCell& cell)
@@ -29,6 +34,7 @@ public:
                 out << cell.toString();
                 return out;
         }
+        virtual unsigned int getId() const { return 0; }
 };
 
 class Wall : public pixelCell
@@ -52,5 +58,49 @@ public:
         std::string toString() const override { return "R"; };
         Road();
 };
+class Start : public pixelCell
+{
+public:
+        virtual ~Start() override = default;
+        std::string toString() const override { return "S"; };
+        Start();
+};
+class Direction : public pixelCell
+{
+public:
+        virtual ~Direction() override = default;
+        std::string toString() const override { return "D"; };
+        Direction();
+};
+class Finish : public pixelCell
+{
+public:
+        virtual ~Finish() override = default;
+        std::string toString() const override { return "E"; };
+        Finish();
+};
+class CheckPoint : public pixelCell
+{
+        int id;
+        // leftmost
+        std::pair<unsigned int, unsigned int> leftmost_point{};
+        // bottem_point
+        std::pair<unsigned int, unsigned int> bottem_point{};
+        // rightmost
+        std::pair<unsigned int, unsigned int> rightmost_point{};
+        // highest_point
+        std::pair<unsigned int, unsigned int> highest_point{};
 
+public:
+        virtual ~CheckPoint() override = default;
+        std::string toString() const override { return "E"; };
+
+        explicit CheckPoint(unsigned int row = 0, unsigned int col = 0, int _id = -1);
+        void updateExtrema(unsigned int row, unsigned int col);
+        unsigned int getId() const override;
+        const std::pair<unsigned int, unsigned int>& getP1() const;
+        const std::pair<unsigned int, unsigned int>& getP2() const;
+        const std::pair<unsigned int, unsigned int>& getP3() const;
+        const std::pair<unsigned int, unsigned int>& getP4() const;
+};
 #endif // RACINGSIMULATOR_PIXELCELL_H
